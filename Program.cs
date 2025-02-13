73bdd6459c07bf94cafe1b00cf62a5b27fa79173
Sheetal <sheetalrana190@gmail.com>
2025-02-13 13:53:57 -0500

    Try to make interactive by asking customer to enter details

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿/*
+* Author: Sheetal Rana
+* Date: 13 Feburary 2025
+* Project: Assignment 2 : Task_1
+* Description: Program that take information from the unlimited number of customers for Electrician. At the end,
+the information is displayed back to the electrician.
+*
+*/
+
+namespace AssignmentTwoTaskOne
+{
+    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter Customer Name: ");
+            string name = Console.ReadLine();
+        }
+    }
+}
