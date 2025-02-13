edce8dce189c59e89e47178cf29696fc61b9b35a
Sheetal <sheetalrana190@gmail.com>
2025-02-13 14:09:13 -0500

    Enter the infoemation for Select Building Type (0: House, 1: Barn, 2: Garage)

--- Program.cs
+++ Program.cs
@@ -14,8 +14,33 @@ namespace AssignmentTwoTaskOne
     {
         static void Main(string[] args)
         {
+            // Enter the name of customer
             Console.Write("Enter Customer Name: ");
             string name = Console.ReadLine();
+
+            // Enter information of type of building
+            Console.WriteLine("Select Building Type (0: House, 1: Barn, 2: Garage): ");
+            int typeInput = int.Parse(Console.ReadLine());
+            if ((typeInput == 0 ) || (typeInput == 1) || (typeInput == 2))
+            {
+                if (typeInput == 0)
+                {
+                    Console.WriteLine("House");
+                }
+                if (typeInput == 1)
+                {
+                    Console.WriteLine("Barn");
+                }
+                if (typeInput == 1)
+                {
+                    Console.WriteLine("Garage");
+                }
+            }
+            else
+            {
+                Console.Write("Invalid input. Please enter 0 (House), 1 (Barn), or 2 (Garage): ");
+            }
+
         }
     }
 }
