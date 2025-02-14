16040ac24222bd95bed7c37c7f014b555e54a726
Sheetal <sheetalrana190@gmail.com>
2025-02-14 15:52:51 -0500

    getting values from customers foe building size, no of bulbes and outlet

--- Program.cs
+++ Program.cs
@@ -14,6 +14,10 @@ namespace AssignmentTwoTaskOne
     {
         static void Main(string[] args)
         {
+            int size = 0;
+            int bulbs = 0;
+            int outlets = 0;
+
             // Enter the name of customer
             Console.Write("Enter Customer Name: ");
             string name = Console.ReadLine();
@@ -31,7 +35,7 @@ namespace AssignmentTwoTaskOne
                 {
                     Console.WriteLine("Barn");
                 }
-                if (typeInput == 1)
+                if (typeInput == 2)
                 {
                     Console.WriteLine("Garage");
                 }
@@ -40,6 +44,38 @@ namespace AssignmentTwoTaskOne
             {
                 Console.Write("Invalid input. Please enter 0 (House), 1 (Barn), or 2 (Garage): ");
             }
+            // Get Building Size
+            
+            Console.Write("Enter Building Size (1000 - 50000 sqft): ");
+            size = int.Parse(Console.ReadLine());
+            if (size >= 1000 && size <= 50000)
+            { 
+                return;
+            }
+            else
+                Console.Write("Invalid size. Please enter a value between 1000 and 50000: ");
+
+            // Get Number of Bulbs
+            
+            Console.Write("Enter Number of Bulbs (Max 20): ");
+            bulbs = int.Parse(Console.ReadLine());
+            if (bulbs >= 0 && bulbs <= 20)
+            {
+                return;
+            }
+            else
+                Console.Write("Invalid number. Enter a value between 0 and 20: ");
+
+            // Get Number of Outlets
+            
+            Console.Write("Enter Number of Outlets (Max 50): ");
+            outlets = int.Parse(Console.ReadLine());
+            if (outlets >= 0 && outlets <= 50)
+            {
+                return; 
+            }
+            else
+                Console.Write("Invalid number. Enter a value between 0 and 50: ");
 
         }
     }
